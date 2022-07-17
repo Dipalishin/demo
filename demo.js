aa1b2e67e904a8d53e93e15371310b24dc10c183
Nitinshin <dipali.shinde14591@gmail.com>
2022-07-17 12:23:19 +0530

    first changes

--- demo.js
+++ demo.js
@@ -9,4 +9,9 @@ for(var i=0;i<items.length;i++)
     items[i].style.fontWeight='bold';
 
 }
+console.log(document.title);
+console.log(document.url);
+console.log(document.head);
+console.log(document.body);
+console.log(document.doctype);
 

5bec3c9144123a942e8300d9b60936d9c8a548cb
Nitinshin <dipali.shinde14591@gmail.com>
2022-07-19 12:10:10 +0530

    Third changes

--- demo.js
+++ demo.js
@@ -1,14 +1,21 @@
 var items=document.getElementsByClassName('list-group-item');
 
-items[3].style.backgroundColor='green';
+//items[3].style.backgroundColor='green';
 
 for(var i=0;i<items.length;i++)
 
 {
-
     items[i].style.fontWeight='bold';
-
+    items[i].style.backgroundColor='#f4f4f4';
 }
+//getelementbytagname
+/*var li=document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='#f4f4f4';
+    li[i].style.fontWeight=' bold ';
+    li[i].style.color='green';
+}*/
 var header=document.getElementById("main-header");
 header.style.border='solid 3px #000';
 var titleItem=document.querySelector('.title');
